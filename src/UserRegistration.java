9580941f9a9a836b3102ce42724ba9bcf9b75d94
shaikhsirin <shaikhsirin8080example.com>
2022-04-05 16:54:47 +0530

    Added:- Lambda First Name using Regex

---
+++ src/UserRegistration.java
@@ -0,0 +1,15 @@
+import java.util.regex.Pattern;
+
+interface Validation {
+    String validate(String regex, String userDetails);
+}
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to lambda user-registation program");
+
+        Validation isValidFirstName = (pattern, firstName) -> {
+            return "First Name is " + Pattern.compile(pattern).matcher(firstName).matches();
+        };
+        System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Sirin"));
+    }
+}
